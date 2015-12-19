d8843dc03fca412c1ac4ff3c64a5dd437707759b
shamful <shamir.khan@islington.gov.uk>
2015-12-19 11:08:25 +0000

    Added main.js to link to reportit using url param

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
+
+$(document).ready(main);
+
+function main(){
+    var enablePOC = getURLParameter('poc');
+    
+    if(enablePOC){
+        alert('success');
+    }
+}
