20fc7737c6a07a07ee84183eb32723beb91a95e0
shamful <shamir.khan@islington.gov.uk>
2015-12-19 16:06:53 +0000

    Revert "Test - add bing road map"
    This reverts commit 50c6133df424fc4187fb536c9353c9a3d78b2171.

--- js/main.js
+++ js/main.js
@@ -4,40 +4,6 @@ function getURLParameter(name) {
 
 $(document).ready(main);
 
-function addBingTiles(){
-    // API key for http://openlayers.org. Please get your own at
-    // http://bingmapsportal.com/ and use that instead.
-    var apiKey = "ApTJzdkyN1DdFKkRAE6QIDtzihNaf6IWJsT-nQ_2eMoO4PN__0Tzhl2-WgJtXFSp";
-
-    var Map1 = new OpenLayers.Map( 'Map1');
-
-    var road = new OpenLayers.Layer.Bing({
-        key: apiKey,
-        type: "Road",
-        // custom metadata parameter to request the new map style - only useful
-        // before May 1st, 2011
-        metadataParams: {mapVersion: "v1"}
-    });
-    var aerial = new OpenLayers.Layer.Bing({
-        key: apiKey,
-        type: "Aerial"
-    });
-    var hybrid = new OpenLayers.Layer.Bing({
-        key: apiKey,
-        type: "AerialWithLabels",
-        name: "Bing Aerial With Labels"
-    });
-
-    Map1.addLayers([road, aerial, hybrid]);
-    Map1.addControl(new OpenLayers.Control.LayerSwitcher());
-    // Zoom level numbering depends on metadata from Bing, which is not yet loaded.
-    var zoom = Map1.getZoomForResolution(76.43702827453613);
-    Map1.setCenter(new OpenLayers.LonLat(-71.147, 42.472).transform(
-        new OpenLayers.Projection("EPSG:4326"),
-        Map1.getProjectionObject()
-    ), zoom);
-}
-
 function main(){
     var enablePOC = getURLParameter('poc');
 
@@ -49,6 +15,5 @@ function main(){
             href: "https://rawgit.com/erronlinex/lbi-reportit-test-scripts/master/css/fixUI.css"
         }).appendTo("head");
 
-        addBingTiles();
     }
 }
